edf74ecb150d1a607ac91be2811bef11c7e7287b
Dev-J0 <dityx.obsidian@gmail.com>
2023-08-25 16:13:00 +0200

    sd

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,41 @@
-import { useState } from 'react'
+import React from 'react';
+import firstSection from './components/firstSection';
+import secondSection from './components/secondSection';
+import thirdSection from './components/thirdSection';
 
-
-import './App.css'
 
 function App() {
-  
+ 
+  return (
+    <div className="App">
+      <h1> CV Application</h1>
+      <h2> Please enter your personal data</h2>
+      <firstSection />
+      <secondSection />
+      <thirdSection />
+    </div>
+  );
+ 
 }
 
-export default App
+export default App;
+
+//Basic info
+//name
+//email
+//phone number
+//submit info button
+
+
+//Educational Experience
+//School name
+//title of study
+//date of study
+//submit info button
+
+//Practical experience
+//company name
+//position title
+//main responsabilities of your jobs
+//date from and until when you worked for that company
+//submit info button
---
+++ src/components/firstSection.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function firstSection() {
+      return (
+            <>
+            <div className="firstS">
+                  <h1>Basic info</h1>
+            </div>
+            </>
+      )
+}
+
+
+
+export default firstSection;
