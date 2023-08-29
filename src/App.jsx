19be2146463fbc318dbedfeb9961d2b11ebc2721
Dev-J0 <dityx.obsidian@gmail.com>
2023-08-29 16:12:24 +0200

    styling with bulma

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import FirstSection from './components/FirstSection';
 import SecondSection from './components/SecondSection';
 import ThirdSection from './components/ThirdSection';
+import './styles/style.css'
 
 
 function App() {
--- src/components/firstSection.jsx
+++ src/components/firstSection.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {useState} from 'react'
 
+
 function FirstSection() {
       const [name, setName] = useState('');
       const [phoneNumber, setPhoneNumber] = useState('');
